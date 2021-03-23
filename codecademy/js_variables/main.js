f58c5b70407f8815ca3337c3b51d7f5af37f0268
SandraLeutner <sandra.leutner@gmx.net>
2021-03-23 20:25:22 +0100

    typeof

--- codecademy/js_variables/main.js
+++ codecademy/js_variables/main.js
@@ -1,3 +1,5 @@
-const myName = 'Sandra';
-const myCity = 'Innsbruck';
-console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+
+newVariable = 1;
+console.log(typeof newVariable);
