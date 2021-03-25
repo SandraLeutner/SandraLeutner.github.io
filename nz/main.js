ca09a5c21cca90ede393a752d08408021d66cc11
SandraLeutner <sandra.leutner@gmx.net>
2021-03-25 16:22:55 +0100

    popup hinzugefügt

--- nz/main.js
+++ nz/main.js
@@ -1,2 +1,13 @@
-console.log("Hello World!");
-console.log(L);
+
+const map = L.map("map", {
+    center: [ -36.833333,174.8  ],
+    zoom: 13,
+    layers: [
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+    ]
+  });
+  
+  let mrk = L.marker([ -36.833333,174.8 ]).addTo(map);
+  mrk.bindPopup('Auckland').openPopup();
+  
+  console.log(document.querySelector("#map"));
