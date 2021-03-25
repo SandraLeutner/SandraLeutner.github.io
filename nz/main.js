4cfb78037298a70a1f87122ecad064c0317035ca
SandraLeutner <sandra.leutner@gmx.net>
2021-03-25 16:34:40 +0100

    stop objekt erstellt

--- nz/main.js
+++ nz/main.js
@@ -1,13 +1,24 @@
+//stop-Objekt erstellen, 6 key value pairs, auf einzelne Elemente des Objekts kann zugegriffen werden
+let stop = {
+    nr: 25,
+    name: 'Auckland',
+    lat: -36.833333,
+    lng: 174.8,
+    user: 'SandraLeutner',
+    wikipedia:'https://en.wikipedia.org/wiki/Auckland'
+};
+
+console.log(stop);
 
 const map = L.map("map", {
-    center: [ -36.833333,174.8  ],
+    center: [ stop.lat, stop.lng  ],
     zoom: 13,
     layers: [
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
     ]
   });
   
-  let mrk = L.marker([ -36.833333,174.8 ]).addTo(map);
+  let mrk = L.marker([ stop.lat, stop.lng ]).addTo(map);
   mrk.bindPopup('Auckland').openPopup();
   
-  console.log(document.querySelector("#map"));
+  //console.log(document.querySelector("#map"));
