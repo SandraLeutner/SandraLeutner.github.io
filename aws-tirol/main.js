70dd131082020b77933de4595f5865d55e18610d
SandraLeutner <sandra.leutner@gmx.net>
2021-04-15 16:42:48 +0200

    add fetch for geodata from ogd.json

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -1,6 +1,6 @@
 let basemapGray = L.tileLayer.provider('BasemapAT.grau');
 
-let map=L.map("map", {
+let map=L.map("map",{
     center: [47, 11],
     zoom: 9,
     layers: [
@@ -8,8 +8,29 @@ let map=L.map("map", {
     ]
 });
 
-//zwischen Layern hin und herschalten
+//zwischen Layern hin und herschalten bzw Layer hinzufügen
 let layerControl = L.control.layers({
     "BasemapAT.grau": basemapGray,
-    "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto')
+    "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto'),
+    "BasemapAT.surface": L.tileLayer.provider('BasemapAT.surface'),
+    "BasemapAT.overlay+ortho": L.layerGroup([
+        L.tileLayer.provider('BasemapAT.orthofoto'),
+        L.tileLayer.provider('BasemapAT.overlay')
+    ])
 }).addTo(map);
+
+let awsUrl = 'https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson';
+
+//Seite im Web laden und auf awsUrl zugreifen
+fetch(awsUrl).then(response => response.json ())
+    .then(json => {
+    console.log('Daten konvertiert: ', json);
+    for (station of json.features){
+        console.log('Station: ', station);
+        let marker = L.marker(
+            [station.geometry.coordinates[1], 
+            station.geometry.coordinates[0]]
+            );
+            marker.addTo(map);
+    }
+});
