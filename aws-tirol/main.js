cd1e61b3508e9251d001cc78368171399cee0fd5
SandraLeutner <sandra.leutner@gmx.net>
2021-04-27 15:13:41 +0200

    getDirections-Funktion in Popup der Stationen einbauen

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -117,7 +117,7 @@ fetch(awsUrl).then(response => response.json())
                 <li>Schneehöhe: ${station.properties.HS || '?'} cm</li>          
                 <li>Luftfeuchtigkeit: ${station.properties.RH || '?'} %</li>
                 <li>Windgeschwindigkeit: ${station.properties.WG || '?'} km/h</li>
-                <li>Windrichtung: ${station.properties.WR || '?'}</li>
+                <li>Windrichtung: ${getDirection(station.properties.WR, DIRECTIONS)}</li>
             </ul>
             <a target="_blank" href="https://wiski.tirol.gv.at/lawine/grafiken/1100/standard/tag/${station.properties.plot}.png">Grafik</a>
             `);
