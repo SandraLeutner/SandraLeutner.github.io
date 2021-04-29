1c31140eaa0b64fc9ac8ed7a0875f0c3079d78c3
SandraLeutner <sandra.leutner@gmx.net>
2021-04-29 18:18:30 +0200

    Sightseeing-Layer hinzugefügt

--- Wien/main.js
+++ Wien/main.js
@@ -1,126 +1,148 @@
-
-
 // OGD-Wien Beispiel
 
 // Kartenhintergründe der basemap.at definieren
 let baselayers = {
-standard: L.tileLayer.provider("BasemapAT.basemap"),
-grau: L.tileLayer.provider("BasemapAT.grau"),
-terrain: L.tileLayer.provider("BasemapAT.terrain"),
-surface: L.tileLayer.provider("BasemapAT.surface"),
-highdpi: L.tileLayer.provider("BasemapAT.highdpi"),
-ortho_overlay: L.layerGroup([
-    L.tileLayer.provider("BasemapAT.orthofoto"),
-    L.tileLayer.provider("BasemapAT.overlay")
-]),
+    standard: L.tileLayer.provider("BasemapAT.basemap"),
+    grau: L.tileLayer.provider("BasemapAT.grau"),
+    terrain: L.tileLayer.provider("BasemapAT.terrain"),
+    surface: L.tileLayer.provider("BasemapAT.surface"),
+    highdpi: L.tileLayer.provider("BasemapAT.highdpi"),
+    ortho_overlay: L.layerGroup([
+        L.tileLayer.provider("BasemapAT.orthofoto"),
+        L.tileLayer.provider("BasemapAT.overlay")
+    ]),
 };
 
 // Overlays für die Themen zum Ein- und Ausschalten definieren
 let overlays = {
-busLines: L.featureGroup(),
-busStops: L.featureGroup(),
-pedAreas: L.featureGroup()
+    busLines: L.featureGroup(),
+    busStops: L.featureGroup(),
+    pedAreas: L.featureGroup(),
+    tourAttr: L.featureGroup()
 };
 
 // Karte initialisieren und auf Wiens Wikipedia Koordinate blicken
 let map = L.map("map", {
-center: [48.208333, 16.373056],
-zoom: 13,
-layers: [
-    baselayers.grau
-]
+    center: [48.208333, 16.373056],
+    zoom: 13,
+    layers: [
+        baselayers.grau
+    ]
 });
 
 // Kartenhintergründe und Overlays zur Layer-Control hinzufügen
 let layerControl = L.control.layers({
-"basemap.at Standard": baselayers.standard,
-"basemap.at grau": baselayers.grau,
-"basemap.at Relief": baselayers.terrain,
-"basemap.at Oberfläche": baselayers.surface,
-"basemap.at hochauflösend": baselayers.highdpi,
-"basemap.at Orthofoto beschriftet": baselayers.ortho_overlay
+    "basemap.at Standard": baselayers.standard,
+    "basemap.at grau": baselayers.grau,
+    "basemap.at Relief": baselayers.terrain,
+    "basemap.at Oberfläche": baselayers.surface,
+    "basemap.at hochauflösend": baselayers.highdpi,
+    "basemap.at Orthofoto beschriftet": baselayers.ortho_overlay
 }, {
-"Liniennetz Vienna Sightseeing": overlays.busLines,
-"Haltestellen Vienna Sightseeing": overlays.busStops,
-"Fußgängerzonen": overlays.pedAreas
+    "Liniennetz Vienna Sightseeing": overlays.busLines,
+    "Haltestellen Vienna Sightseeing": overlays.busStops,
+    "Fußgängerzonen": overlays.pedAreas,
+    "Sehenswürdigkeiten": overlays.tourAttr
 }).addTo(map);
 
 // alle Overlays nach dem Laden anzeigen
 overlays.busLines.addTo(map);
 overlays.busStops.addTo(map);
 overlays.pedAreas.addTo(map);
+overlays.tourAttr.addTo(map);
 
 let drawBusStop = (geojsonData) => {
-L.geoJson(geojsonData, {
-    onEachFeature: (feature, layer) => {
-        layer.bindPopup(`<strong>${feature.properties.LINE_NAME}</strong>
+    L.geoJson(geojsonData, {
+        onEachFeature: (feature, layer) => {
+            layer.bindPopup(`<strong>${feature.properties.LINE_NAME}</strong>
         <hr>
         Station: ${feature.properties.STAT_NAME}`)
-    },
-    pointToLayer: (geoJsonPoint, latlng) => {
-        return L.marker(latlng, {
-            icon: L.icon({
-                iconUrl: 'icons/busstop.png',
-                iconSize: [38, 38]
+        },
+        pointToLayer: (geoJsonPoint, latlng) => {
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: 'icons/busstop.png',
+                    iconSize: [35, 35]
+                })
             })
-        })
-    },
-    attribution: '<a href="https://data.wien.gv.at">Stadt Wien</a>, <a href="https://mapicons.mapsmarker.com">Maps Icons Collection</a>'
-}).addTo(overlays.busStops);
+        },
+        attribution: '<a href="https://data.wien.gv.at">Stadt Wien</a>, <a href="https://mapicons.mapsmarker.com">Maps Icons Collection</a>'
+    }).addTo(overlays.busStops);
 }
 
 let drawBusLines = (geojsonData) => {
-console.log('Bus Lines: ', geojsonData);
-L.geoJson(geojsonData, {
-    style: (feature) => {
-        let col = COLORS.buslines[feature.properties.LINE_NAME];
-        return {
-            color: col
-        }
-    },
-    onEachFeature: (feature, layer) => {
-        layer.bindPopup(`<strong>${feature.properties.LINE_NAME}</strong>
+    console.log('Bus Lines: ', geojsonData);
+    L.geoJson(geojsonData, {
+        style: (feature) => {
+            let col = COLORS.buslines[feature.properties.LINE_NAME];
+            return {
+                color: col
+            }
+        },
+        onEachFeature: (feature, layer) => {
+            layer.bindPopup(`<strong>${feature.properties.LINE_NAME}</strong>
         <hr>
         von ${feature.properties.FROM_NAME}<br>
         nach ${feature.properties.TO_NAME}`)
-    }
-}).addTo(overlays.busLines);
+        }
+    }).addTo(overlays.busLines);
 }
 
 let drawPedestrianAreas = (geojsonData) => {
-console.log('Zone: ', geojsonData);
-L.geoJson(geojsonData, {
-    style: (feature) => {
-        return {
-            stroke: true,
-            color: "silver",
-            fillColor: "yellow",
-            fillOpacity: 0.3
-        }
-    },
-    onEachFeature: (feature, layer) => {
-        layer.bindPopup(`<strong>Fußgängerzone ${feature.properties.ADRESSE}</strong>
+    console.log('Zone: ', geojsonData);
+    L.geoJson(geojsonData, {
+        style: (feature) => {
+            return {
+                stroke: true,
+                color: "silver",
+                fillColor: "yellow",
+                fillOpacity: 0.3
+            }
+        },
+        onEachFeature: (feature, layer) => {
+            layer.bindPopup(`<strong>Fußgängerzone ${feature.properties.ADRESSE}</strong>
         <hr>
         ${feature.properties.ZEITRAUM} <br>
         ${feature.properties.AUSN_TEXT}
         `);
-    }
-}).addTo(overlays.pedAreas);
+        }
+    }).addTo(overlays.pedAreas);
+}
+
+let drawTouristAttr = (geojsonData) => {
+    console.log('Attraktion: ', geojsonData);
+    L.geoJson(geojsonData, {
+        onEachFeature: (feature, layer) => {
+            layer.bindPopup(`<strong>Sehenswürdigkeit ${feature.properties.NAME}</strong>
+            <hr>
+            Adresse: ${feature.properties.ADRESSE}`);
+        },
+
+        pointToLayer: (geoJsonPoint, latlng) => {
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: 'icons/sightseeing.png',
+                    iconSize: [35, 35]
+                })
+            })
+        },
+    }).addTo(overlays.tourAttr);
 }
 
 for (let config of OGDWIEN) {
-// console.log("Config: ", config.data);
-fetch(config.data)
-    .then(response => response.json())
-    .then(geojsonData => {
-        // console.log("Data: ", geojsonData);
-        if (config.title == "Haltestellen Vienna Sightseeing") {
-            drawBusStop(geojsonData);
-        } else if (config.title == "Liniennetz Vienna Sightseeing") {
-            drawBusLines(geojsonData);
-        } else if (config.title === "Fußgängerzonen") {
-            drawPedestrianAreas(geojsonData);
-        }
-    })
+    // console.log("Config: ", config.data);
+    fetch(config.data)
+        .then(response => response.json())
+        .then(geojsonData => {
+            // console.log("Data: ", geojsonData);
+            if (config.title == "Haltestellen Vienna Sightseeing") {
+                drawBusStop(geojsonData);
+            } else if (config.title == "Liniennetz Vienna Sightseeing") {
+                drawBusLines(geojsonData);
+            } else if (config.title === "Fußgängerzonen") {
+                drawPedestrianAreas(geojsonData);
+            } else if (config.title == "Sehenswürdigkeiten") {
+                drawTouristAttr(geojsonData);
+            }
+        })
 }
-
